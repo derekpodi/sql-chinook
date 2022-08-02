9b484b4487c017420508205407ab90b63fa6aa71
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-08-01 20:27:29 -0700

    Add files via upload

--- homework6.sql
+++ homework6.sql
@@ -1,5 +1,17 @@
 --Derek Podimatis
 
-USE Chinook
+USE master
+IF DB_ID('MyDB_DerekPodimatis') IS NOT NULL
+BEGIN
+    ALTER DATABASE MyDB_DerekPodimatis SET OFFLINE WITH ROLLBACK IMMEDIATE;
+    ALTER DATABASE MyDB_DerekPodimatis SET ONLINE;
+    DROP DATABASE MyDB_DerekPodimatis;
+END
+
 
 --1
+CREATE DATABASE MyDB_DerekPodimatis
+GO
+USE MyDB_DerekPodimatis
+
+--2
