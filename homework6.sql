b8b367bdf36b7ef7aa8cf197aaf4ac9a1d1edb74
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-08-02 09:35:38 -0700

    Add files via upload

--- homework6.sql
+++ homework6.sql
@@ -15,3 +15,71 @@ GO
 USE MyDB_DerekPodimatis
 
 --2
+SELECT *
+INTO Users
+FROM Chinook.dbo.Customer
+USE MyDB_DerekPodimatis
+
+
+--3
+DELETE Users
+WHERE CustomerId % 2 != 0
+
+
+--4
+UPDATE Users
+SET Company = CASE
+    WHEN Email LIKE '%gmail%' THEN 'Google'
+    WHEN Email LIKE '%yahoo%' THEN 'Yahoo!'
+    ELSE Company
+    END
+
+
+--5
+EXEC sp_rename 'Users.CustomerId', 'UserId', 'COLUMN'
+
+
+--6
+--ALTER TABLE Users
+--DROP CONSTRAINT pk_Users
+
+ALTER TABLE Users
+ADD CONSTRAINT pk_Users PRIMARY KEY (UserId)
+
+
+--7
+CREATE TABLE Address (
+    AddressId int IDENTITY(1,1) PRIMARY KEY
+    ,AddressType varchar(10)
+    ,AddressLine1 varchar(50)
+    ,City varchar(30)
+    ,State varchar(2)
+    ,UserId int
+    ,CreateDate datetime DEFAULT GETDATE()
+)
+
+
+--8
+ALTER TABLE Address
+ADD CONSTRAINT uc_AddressType UNIQUE (UserId, AddressType)
+
+
+--9
+ALTER TABLE Address
+ADD CONSTRAINT fk_UserAddress FOREIGN KEY (UserId)
+    REFERENCES Users(UserId)
+
+
+--10
+INSERT INTO Address (AddressType, AddressLine1, City, State, UserId)
+VALUES ('home', '111 Elm St.', 'Los Angeles', 'CA', 2)
+,('home', '222 Palm Ave.', 'San Diego', 'CA', 4)
+,('work', '333 Oak Ln.', 'La Jolla', 'CA', 4)
+
+
+--11
+SELECT *
+FROM Users
+
+SELECT *
+FROM Address
