38b228e6bdbabd3f40de704315fa4f4cf5769294
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-09-29 08:57:31 -0700

    Add files via upload

--- Intermediate/homework2.sql
+++ Intermediate/homework2.sql
@@ -20,7 +20,72 @@ JOIN Genre G
 JOIN MediaType MT
     ON MT.MediaTypeId = T.MediaTypeId
 
+/*
+SELECT *
+FROM Track_v_dp
+*/
 
 GO
 
 --2
+CREATE FUNCTION ArtistAlbum_fn_dp (@TrackId int)
+RETURNS varchar(100)
+AS
+BEGIN
+DECLARE @ArtistAlbum varchar(100)
+SELECT
+    @ArtistAlbum = CONCAT(A.Name, '-', AL.Title)
+FROM Track T
+JOIN Album AL
+    ON AL.AlbumId = T.AlbumId
+JOIN Artist A
+    ON A.ArtistId = AL.ArtistId
+WHERE T.TrackId = @TrackId
+RETURN
+    @ArtistAlbum
+END
+
+/*
+SELECT
+    dbo.ArtistAlbum_fn_dp(TrackId)
+FROM Track
+*/
+
+GO
+
+--3
+CREATE PROC TracksByArtist_p_dp @ArtistName varchar(100) AS
+SELECT
+    A.Name AS ArtistName
+    ,AL.Title AS AlbumTitle
+    ,T.Name AS TrackName
+FROM Artist A
+JOIN Album AL 
+    ON AL.ArtistId = A.ArtistId
+JOIN Track T
+    ON T.AlbumId = AL.AlbumId
+WHERE A.Name LIKE CONCAT(@ArtistName,'%')
+
+
+/*
+EXEC TracksByArtist_p_dp 'Bl'
+*/
+
+GO
+
+--4
+SELECT
+    T.Name
+    ,T.GenreName
+    ,T.MediaTypeName
+FROM Track_v_dp T
+JOIN Album AL 
+    ON AL.AlbumId = T.AlbumId
+WHERE T.Name = 'Babylon'
+
+
+GO
+
+--5
+
+
