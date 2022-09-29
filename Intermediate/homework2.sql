fe3c2b2502cddf99cb9894bd01ad1e5b1e9330c0
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-09-28 19:24:28 -0700

    Add files via upload

--- Intermediate/homework2.sql
+++ Intermediate/homework2.sql
@@ -1,9 +1,26 @@
 --Derek Podimatis
 
 USE Chinook
-IF OBJECT_ID('Track_v_edw') IS NOT NULL DROP VIEW Track_v_dp
-IF OBJECT_ID('ArtistAlbum_fn_edw') IS NOT NULL DROP FUNCTION ArtistAlbum_fn_dp
-IF OBJECT_ID('TracksByArtist_p_edw') IS NOT NULL DROP PROC TracksByArtist_p_dp
+IF OBJECT_ID('Track_v_dp') IS NOT NULL DROP VIEW Track_v_dp
+IF OBJECT_ID('ArtistAlbum_fn_dp') IS NOT NULL DROP FUNCTION ArtistAlbum_fn_dp
+IF OBJECT_ID('TracksByArtist_p_dp') IS NOT NULL DROP PROC TracksByArtist_p_dp
 
+
+GO
 
 --1
+CREATE VIEW Track_v_dp AS
+SELECT
+    T.*
+    ,G.Name AS GenreName
+    ,MT.Name AS MediaTypeName
+FROM Track T
+JOIN Genre G
+    ON G.GenreId = T.GenreId
+JOIN MediaType MT
+    ON MT.MediaTypeId = T.MediaTypeId
+
+
+GO
+
+--2
