159dedc753f71fdaa6885e363bb60a17e9713e19
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-09-28 17:05:21 -0700

    Add files via upload

--- Intermediate/homework2.sql
+++ Intermediate/homework2.sql
@@ -1,3 +1,9 @@
 --Derek Podimatis
 
+USE Chinook
+IF OBJECT_ID('Track_v_edw') IS NOT NULL DROP VIEW Track_v_dp
+IF OBJECT_ID('ArtistAlbum_fn_edw') IS NOT NULL DROP FUNCTION ArtistAlbum_fn_dp
+IF OBJECT_ID('TracksByArtist_p_edw') IS NOT NULL DROP PROC TracksByArtist_p_dp
+
+
 --1
