2c16c16db5ed9ea0dfd7d76940e86bc5aa540543
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-10-07 14:13:47 -0700

    Add files via upload

--- Intermediate/homework3.sql
+++ Intermediate/homework3.sql
@@ -2,3 +2,72 @@
 
 
 --1
+WITH AA AS
+(
+SELECT
+    A.ArtistId
+    ,A.NAME AS ArtistName
+    ,AL.AlbumId
+    ,AL.Title AS AlbumTitle
+FROM Artist A
+JOIN ALBUM AL
+    ON AL.ArtistId = A.ArtistId
+WHERE A.NAME = 'AudioSlave'
+)
+SELECT
+    ArtistName
+    ,AlbumTitle
+    ,T.Name AS TrackName
+FROM AA
+JOIN Track T
+    ON T.AlbumId = AA.AlbumId
+
+
+--2
+SELECT
+    ArtistName
+    ,AlbumTitle
+    ,T.Name AS TrackName
+FROM
+(
+    SELECT
+        A.ArtistId
+        ,A.NAME AS ArtistName
+        ,AL.AlbumId
+        ,AL.Title AS AlbumTitle
+    FROM Artist A
+    JOIN ALBUM AL
+        ON AL.ArtistId = A.ArtistId
+    WHERE A.NAME = 'Kiss'
+) AS AA
+JOIN Track T
+    ON T.AlbumId = AA.AlbumId
+
+
+--3
+WITH CustomerInvoice AS
+(
+SELECT
+    CONCAT(C.FirstName, ' ', C.LastName) AS CustomerName
+    ,SUM(I.Total) AS SumTotal
+    ,C.SupportRepId
+FROM Customer C
+JOIN Invoice I
+    ON I.CustomerId = C.CustomerId
+GROUP BY C.FirstName, C.LastName, C.SupportRepId
+)
+SELECT
+    E.LastName AS SupportRep
+    ,CustomerName
+    ,SumTotal
+FROM CustomerInvoice
+JOIN Employee E
+    ON E.EmployeeId = CustomerInvoice.SupportRepId
+ORDER BY SumTotal DESC, SupportRep
+
+
+--4
+
+
+
+
