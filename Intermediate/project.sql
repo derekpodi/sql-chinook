4be532b45c990ff513c02954ece38ba771cec048
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-11-13 21:03:42 -0800

    Add files via upload

--- Intermediate/project.sql
+++ Intermediate/project.sql
@@ -9,6 +9,8 @@ USE LSP_dp
 GO
 */
 
+
+
 /*
 -------- CREATE TABLES --------
 
@@ -55,5 +57,103 @@ CREATE TABLE FacultyPayment(
 CREATE TABLE Faculty(
     FacultyID int PRIMARY KEY
 )
+
+*/
+
+
+
+/*
+--------ADDITIONAL DATABASE OBJECTS--------
+--1.
+CREATE VIEW CourseRevenue_v AS
+SELECT
+    [Course Code]
+    ,[Course Title]
+    ,[Count of Sections]
+    ,[Total Gross Revenue]
+    ,[AVG Revenue Per Section]
+FROM [TABLE NAME]
+
+--2.
+CREATE VIEW AnnualRevenue_v AS
+SELECT
+    [Gross Revenue from tuition]
+    ,[faculty payments fr each academic year]
+FROM [TABLE NAME]
+
+--3.
+CREATE PROC StudentHistory_p @PersonPrimaryKey int AS
+SELECT
+    [Student Name]
+    ,[SectionID]
+    ,[Course Code]
+    ,[CourseTitle]
+    ,[Primary Instructor Name]
+    ,[Term Code]
+    ,[Start Date]
+    ,[Tuition Paid]
+    ,[Grade]
+FROM [TABLE NAME]
+WHERE PersonID = @PersonPrimaryKey
+
+--4.
+CREATE PROC InsertPerson_p
+    @PersonFirstName varchar(50)
+    ,@PersonLastName varchar(50)
+    ,@AddressAddressType varchar(50)
+    ,@AddressAddressLine varchar(50)
+    ,@AddressCity varchar(50) AS
+BEGIN
+    INSERT INTO Person(FirstName, LastName)
+    VALUES(@PersonFirstName, @PersonLastName);
+
+    INSERT INTO Address(AddressType, AddressLine, City)
+    VALUES(@AddressAddressType, @AddressAddressLine, @AddressCity);
+
+END
+
+*/
+
+
+
+/*
+---------EXECUTE CODE FOR SCREENSHOTS--------
+--1
+SELECT * FROM CourseRevenue_v ORDER BY CourseCode
+--2
+SELECT * FROM AnnualRevenue_v ORDER BY AcademicYear
+--3
+EXEC StudentHistory_p 1400
+--4
+EXEC InsertPerson_p 'Eric','Williamson','work','500 Elm St.','North Pole'
+
+SELECT TOP 1 * FROM Person ORDER BY PersonID DESC
+SELECT TOP 1 * FROM Address ORDER BY AddressID DESC
+
+/*
+
+
+
+/*
+-------- DROP OBJECTS --------
+DROP TABLE Section
+
+DROP TABLE Course
+
+DROP TABLE Room
+
+DROP TABLE Term
+
+DROP TABLE ClassList
+
+DROP TABLE Person
+
+DROP TABLE Address
+
+DROP TABLE FacultyPayment
+
+DROP TABLE Faculty
+
+DROP DATABASE LSP_dp
 
 */
