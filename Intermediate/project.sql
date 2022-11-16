add8ee77e29f509fb24640a9f67a2fe639d278bb
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-11-15 19:05:23 -0800

    Add files via upload

--- Intermediate/project.sql
+++ Intermediate/project.sql
@@ -1,66 +1,285 @@
-USE master 
+
+--------DROP DATABSE TO START CLEAN FOR EACH RUN ------
+USE master
+IF DB_ID('LSP_dp') IS NOT NULL
+BEGIN
+    ALTER DATABASE LSP_dp SET OFFLINE WITH ROLLBACK IMMEDIATE;
+    ALTER DATABASE LSP_dp SET ONLINE;
+    DROP DATABASE LSP_dp;
+END
+
 GO
 
 
 -------- CREATE DATABASES --------
-CREATE DATABASE LSP_stage
-GO
-
 CREATE DATABASE LSP_dp
 GO
-
 USE LSP_dp 
-GO
 
 
-/*
 -------- CREATE TABLES --------
+CREATE TABLE Course(
+    CourseID int PRIMARY KEY IDENTITY(1,1)
+    ,CourseCode varchar(50)
+    ,CourseTitle varchar(100)
+    ,TotalWeeks SMALLINT
+    ,TotalHours FLOAT
+    ,FullCourseFee SMALLINT
+    ,CourseDescription varchar(350)
+)
+
+CREATE TABLE Room(
+    RoomID int PRIMARY KEY IDENTITY(1,1)
+    ,RoomName varchar(50)
+    ,Capacity SMALLINT
+)
+
+CREATE TABLE Term(
+    TermID int PRIMARY KEY
+    ,TermCode varchar(50)
+    ,TermName varchar(50)
+    ,CalendarYear SMALLINT
+    ,AcademicYear SMALLINT
+)
 
 CREATE TABLE Section(
     SectionID int PRIMARY KEY
     ,CourseID int FOREIGN KEY REFERENCES Course(CourseID)
     ,TermID int FOREIGN KEY REFERENCES Term(TermID)
     ,RoomID int FOREIGN KEY REFERENCES Room(RoomID)
-)
-
-CREATE TABLE Course(
-    CourseID int PRIMARY KEY
-)
-
-CREATE TABLE Room(
-    RoomID int PRIMARY KEY
-)
-
-CREATE TABLE Term(
-    TermID int PRIMARY KEY
-)
-
-CREATE TABLE ClassList(
-    ClassListID int PRIMARY KEY
-    ,SectionID int FOREIGN KEY REFERENCES Section(SectionID)
-    ,PersonID int FOREIGN KEY REFERENCES Person(PersonID)
+    ,CourseCode varchar(50)
+    ,CourseTitle varchar(50)
+    ,TermCode varchar(50)
+    ,StartDate DATE
+    ,EndDate DATE
+    ,Days varchar(50)
+    ,SectionStatus varchar(50)
+    ,RoomName varchar(50)
+    ,PrimaryInstructor varchar(50)
+    ,PrimaryPayment INT
+    ,SecondaryInstructor varchar(50)
+    ,SecondaryPayment INT
 )
 
 CREATE TABLE Person(
     PersonID int PRIMARY KEY
+    ,LastName varchar(50)
+    ,FirstName varchar(50)
+    ,MiddleName varchar(50)
+    ,Gender varchar(50)
+    ,Phone varchar(50)
+    ,Email varchar(50)
+    ,AddressLine varchar(50)
+    ,City varchar(50)
+    ,State varchar(50)
+    ,PostalCode varchar(50)
 )
 
 CREATE TABLE Address(
-    AddressID int PRIMARY KEY
+    AddressID int PRIMARY KEY IDENTITY(1,1)
     ,PersonID int FOREIGN KEY REFERENCES Person(PersonID)
+    ,AddressType varchar(50)
+    ,AddressLine varchar(50)
+    ,City varchar(50)
+    ,State varchar(50)
+    ,PostalCode varchar(50)
+)
+
+CREATE TABLE ClassList(
+    ClassListID int PRIMARY KEY IDENTITY(1,1)
+    ,SectionID int FOREIGN KEY REFERENCES Section(SectionID)
+    ,PersonID int FOREIGN KEY REFERENCES Person(PersonID)
+    ,LastName varchar(50)
+    ,FirstName varchar(50)
+    ,Phone varchar(50)
+    ,Email varchar(50)
+    ,EnrollmentStatus varchar(50)
+    ,TuitionAmount int
+    ,Grade varchar(50)
+    ,TermID int FOREIGN KEY REFERENCES Term(TermID)
+)
+
+CREATE TABLE Faculty(
+    FacultyID int PRIMARY KEY IDENTITY(1,1)
+    ,FacultyFirstName varchar(50)
+    ,FacultyLastName varchar(50)
+    ,FacultyEmail varchar(50)
+    ,PrimaryPhone varchar(50)
+    ,AlternativePhone varchar(50)
+    ,FacultyAddressLine varchar(50)
+    ,FacultyCity varchar(50)
+    ,FacultyState varchar(50)
+    ,FacultyPostalCode int
 )
 
 CREATE TABLE FacultyPayment(
-    FacultyPaymentID int PRIMARY KEY
+    FacultyPaymentID int PRIMARY KEY IDENTITY(1,1)
     ,FacultyID int FOREIGN KEY REFERENCES Faculty(FacultyID)
     ,SectionID int FOREIGN KEY REFERENCES Section(SectionID)
 )
 
-CREATE TABLE Faculty(
-    FacultyID int PRIMARY KEY
-)
 
-*/
+
+--------INSERT DATA--------
+--Course
+INSERT INTO [LSP_dp]..Course
+(
+  [CourseCode]
+  ,[CourseTitle]
+  ,[TotalWeeks]
+  ,[TotalHours]
+  ,[FullCourseFee]
+  ,[CourseDescription]
+)
+SELECT [CourseCode], [CourseTitle], [TotalWeeks], [TotalHours], [FullCourseFee], [CourseDescription]
+FROM [LSP_stage]..[Courses15]
+UNION
+SELECT [CourseCode], [CourseTitle], [TotalWeeks], [TotalHours], [FullCourseFee], [CourseDescription]
+FROM [LSP_stage]..[Courses19]
+
+
+--Room
+INSERT INTO [LSP_dp]..Room
+(
+  [RoomName]
+  ,[Capacity]
+)
+SELECT [RoomName], [Capacity]
+FROM [LSP_stage]..[Rooms15]
+UNION
+SELECT [RoomName], [Capacity]
+FROM [LSP_stage]..[Rooms19]
+
+
+--Term
+INSERT INTO [LSP_dp]..Term
+(
+  [TermID]
+  ,[TermCode]
+  ,[TermName]
+  ,[CalendarYear]
+  ,[AcademicYear]
+)
+SELECT [TermID], [TermCode], [TermName], [CalendarYear], [AcademicYear]
+FROM [LSP_stage]..[Terms15]
+UNION
+SELECT [TermID], [TermCode], [TermName], [CalendarYear], [AcademicYear]
+FROM [LSP_stage]..[Terms19]
+
+
+--Section
+INSERT INTO [LSP_dp]..Section
+(
+  [SectionID]
+  ,[CourseCode]
+  ,[TermCode]
+  ,[StartDate]
+  ,[EndDate]
+  ,[Days]
+  ,[SectionStatus]
+  ,[RoomName]
+  ,[PrimaryInstructor]
+  ,[PrimaryPayment]
+  ,[SecondaryInstructor]
+  ,[SecondaryPayment]
+)
+SELECT [SectionID], [CourseCode], [TermCode], [StartDate], [EndDate], [Days], [SectionStatus], [RoomName], [PrimaryInstructor], [PrimaryPayment], [SecondaryInstructor], [SecondaryPayment]
+FROM [LSP_stage]..[Sections SU11-SU15]
+UNION
+SELECT [SectionID], [CourseCode], [TermCode], [StartDate], [EndDate], [Days], [SectionStatus], [RoomName], [PrimaryInstructor], [PrimaryPayment], [SecondaryInstructor], [SecondaryPayment]
+FROM [LSP_stage]..[Sections FA15-SU19]
+
+
+--Person
+INSERT INTO [LSP_dp]..Person
+(
+  [PersonID]
+  ,[LastName]
+  ,[FirstName]
+  ,[MiddleName]
+  ,[Gender]
+  ,[Phone]
+  ,[Email]
+  ,[AddressLine]
+  ,[City]
+  ,[State]
+  ,[PostalCode]
+)
+SELECT [PersonID], [LastName], [FirstName], [MiddleName], [Gender], [Phone], [Email], [AddressLine], [City], [State], [PostalCode]
+FROM [LSP_stage]..[Persons15]
+UNION
+SELECT [PersonID], [LastName], [FirstName], [MiddleName], [Gender], [Phone], [Email], [AddressLine], [City], [State], [PostalCode]
+FROM [LSP_stage]..[Persons19]
+
+
+--Address
+INSERT INTO [LSP_dp]..Address
+(
+  [AddressType]
+  ,[AddressLine]
+  ,[City]
+  ,[State]
+  ,[PostalCode]
+)
+SELECT 'home' AS [AddressType], [AddressLine], [City], [State], [PostalCode]
+FROM [LSP_stage]..[Persons15]
+UNION
+SELECT 'home' AS [AddressType], [AddressLine], [City], [State], [PostalCode]
+FROM [LSP_stage]..[Persons19]
+
+
+--ClassList
+INSERT INTO [LSP_dp]..ClassList
+(
+  [SectionID]
+  ,[PersonID]
+  ,[LastName]
+  ,[FirstName]
+  ,[Phone]
+  ,[Email]
+  ,[EnrollmentStatus]
+  ,[TuitionAmount]
+  ,[Grade]
+  ,[TermID]
+)
+SELECT [SectionID], [PersonID], [LastName], [FirstName], [Phone], [Email], [EnrollmentStatus], [TuitionAmount], [Grade], [TermID]
+FROM [LSP_stage]..[Classlist SU11-SU15]
+UNION
+SELECT [SectionID], [PersonID], [LastName], [FirstName], [Phone], [Email], [EnrollmentStatus], [TuitionAmount], [Grade], [TermID]
+FROM [LSP_stage]..[ClassList FA15-SU19]
+
+
+--Faculty
+INSERT INTO [LSP_dp]..Faculty
+(
+  [FacultyFirstName]
+  ,[FacultyLastName]
+  ,[FacultyEmail]
+  ,[PrimaryPhone]
+  ,[AlternativePhone]
+  ,[FacultyAddressLine]
+  ,[FacultyCity]
+  ,[FacultyState]
+  ,[FacultyPostalCode]
+)
+SELECT [FacultyFirstName], [FacultyLastName], [FacultyEmail], [PrimaryPhone], [AlternatePhone], [FacultyAddressLine], [FacultyCity], [FacultyState], [FacultyPostalCode]
+FROM [LSP_stage]..[Faculty15]
+UNION
+SELECT [FacultyFirstName], [FacultyLastName], [FacultyEmail], [PrimaryPhone], [AlternatePhone], [FacultyAddressLine], [FacultyCity], [FacultyState], [FacultyPostalCode]
+FROM [LSP_stage]..[Faculty19]
+
+
+--FacultyPayment
+INSERT INTO [LSP_dp]..FacultyPayment
+(
+  [FacultyID]
+)
+SELECT [FacultyID]
+FROM [LSP_dp]..[Faculty]
+UNION
+SELECT [FacultyID]
+FROM [LSP_dp]..[Faculty]
+
 
 
 
@@ -136,7 +355,7 @@ SELECT TOP 1 * FROM Address ORDER BY AddressID DESC
 
 
 
-
+/*
 -------- DROP OBJECTS --------
 DROP TABLE Section
 
@@ -157,7 +376,7 @@ DROP TABLE FacultyPayment
 DROP TABLE Faculty
 
 DROP DATABASE LSP_dp;
-
+*/
 
 
 
@@ -167,3 +386,87 @@ DROP DATABASE LSP_dp;
 --https://stackoverflow.com/questions/53293349/azure-data-studio-schema-diagram
 --https://www.sqlshack.com/sql-server-data-import-using-azure-data-studio/
 
+
+/*
+-------WORKING AREA------
+USE LSP_stage
+
+--Max Col Length for CourseDescription(301)
+SELECT TOP (100)
+    [CourseDescription]
+    ,LEN([CourseDescription])
+FROM [LSP_stage].[dbo].[Courses15]
+ORDER BY LEN([CourseDescription]) DESC
+
+SELECT TOP (100)
+    [CourseTitle]
+    ,LEN([CourseTitle])
+FROM [LSP_stage].[dbo].[Courses19]
+ORDER BY LEN([CourseTitle]) DESC
+
+
+--Rooms
+SELECT *
+FROM [Rooms15]
+UNION
+SELECT *
+FROM [Rooms19]
+
+--TERM
+SELECT *
+FROM [Terms15]
+UNION
+SELECT *
+FROM [Terms19]
+
+--Courses
+SELECT *
+FROM [Courses15]
+UNION
+SELECT *
+FROM [Courses19]
+
+--Sections
+SELECT *
+FROM [Sections SU11-SU15]
+UNION
+SELECT *
+FROM [Sections FA15-SU19]
+
+--Persons
+SELECT *
+FROM [Persons15]
+UNION
+SELECT *
+FROM [Persons19]
+
+--ClassList
+SELECT *
+FROM [Classlist SU11-SU15]
+UNION
+SELECT *
+FROM [ClassList FA15-SU19]
+
+--Faculty
+SELECT *
+FROM [Faculty15]
+UNION
+SELECT *
+FROM [Faculty19]
+
+
+WITH CTE AS(
+    SELECT *
+    FROM [Sections SU11-SU15]
+    UNION
+    SELECT *
+    FROM [Sections FA15-SU19]
+)
+SELECT
+    SectionID
+    ,COUNT(SectionID)
+FROM CTE
+GROUP BY SectionID
+ORDER BY COUNT(SectionID) DESC
+
+*/
