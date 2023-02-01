c09f7d1f65517f75d390dd8c9d0b41b91e5a0752
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2023-01-31 19:19:31 -0800

    Add files via upload

--- Advanced/homework3B.sql
+++ Advanced/homework3B.sql
@@ -4,10 +4,10 @@ USE Chinook
 
 
 --1
-PRINT('First Statement')
+PRINT 'First Statement'
 RETURN
-PRINT('Second Statement')
-PRINT('Third Statement')
+PRINT 'Second Statement'
+PRINT 'Third Statement'
 GO
 
 
@@ -43,24 +43,22 @@ BEGIN
 PRINT CONCAT('Counter Number: ', @Counter)
 SET @Counter += 1
 END
-PRINT('End of Loop.')
+PRINT 'End of Loop.'
 GO
 
 
 --5
 DECLARE @EmployeeName AS table (FullName nvarchar(50))
-DECLARE @FullName AS nvarchar(50)
 DECLARE @Counter int = 1
 DECLARE @MaxId int
-SELECT @MaxId = COUNT(*) FROM Employee
-WHILE (@Counter <= @MaxId)
+SELECT @MaxId = MAX(EmployeeId) FROM Employee
+WHILE @Counter <= @MaxId
 BEGIN
+INSERT INTO @EmployeeName
 SELECT
-    @FullName = CONCAT(FirstName, ' ', LastName)
+    CONCAT(FirstName, ' ', LastName)
 FROM Employee
 WHERE EmployeeId = @Counter
-INSERT INTO @EmployeeName
-VALUES(@FullName)
 SET @Counter += 1
 END
 SELECT * FROM @EmployeeName
@@ -71,13 +69,15 @@ GO
 DECLARE @SumTotal numeric(9,2) = 0
 DECLARE @Counter int = 1
 DECLARE @MaxId int
-SELECT @MaxId = COUNT(*) FROM Invoice
-WHILE (@Counter <= @MaxId)
+SELECT @MaxId = MAX(InvoiceId) FROM Invoice
+WHILE @Counter <= @MaxId
 BEGIN
-SELECT 
-    @SumTotal += Total 
-FROM Invoice
-WHERE Total < 1.00 AND InvoiceId = @Counter
+IF (SELECT Total FROM Invoice WHERE InvoiceId = @Counter) > 0.99
+BEGIN
+    SET @Counter += 1
+    CONTINUE 
+END
+SET @SumTotal += (SELECT Total FROM Invoice WHERE InvoiceId = @Counter)
 SET @Counter += 1
 END
 SELECT @SumTotal AS [Sum Total of Invoices less than a Dollar]
@@ -85,67 +85,62 @@ GO
 
 
 --7
-DECLARE @RunningTable AS table (Amount numeric(9,2), RollingTotal numeric(9,2), Note nvarchar(50))
-DECLARE @Amount numeric(9,2) = 0
-DECLARE @RollingTotal numeric(9,2) = 0
-DECLARE @Note nvarchar(50)
+DECLARE @RollingTable AS table (Amount numeric(9,2), RollingTotal numeric(9,2), Note nvarchar(50))
+DECLARE @Sum numeric(9,2) = 0
 DECLARE @Counter int = 1
 DECLARE @MaxId int
-SELECT @MaxId = COUNT(*) FROM Invoice
+SELECT @MaxId = MAX(InvoiceId) FROM Invoice
 WHILE (@Counter <= @MaxId)
 BEGIN
+Set @Sum += (SELECT Total FROM Invoice WHERE InvoiceId = @Counter)
+INSERT INTO @RollingTable(Amount, RollingTotal, Note)
 SELECT
-    @Amount = Total
-    ,@RollingTotal += Total
-    ,@Note = CONCAT('InvoiceId: ', @Counter)
-FROM Invoice I
+    Total
+    ,@Sum
+    ,CONCAT('InvoiceId: ',@Counter)
+FROM Invoice
 WHERE InvoiceId = @Counter
-IF @RollingTotal >= 50
-    BEGIN
-    INSERT INTO @RunningTable
-    VALUES(@Amount, @RollingTotal, 'Total greater than 50.')
+IF @Sum > 50
+BEGIN
+    UPDATE @RollingTable
+    SET Note = 'Total greater than 50.'
+    WHERE RollingTotal = @Sum    
     BREAK
-    END
-INSERT INTO @RunningTable
-VALUES(@Amount, @RollingTotal, @Note)
+END
 SET @Counter += 1
 END
-SELECT * FROM @RunningTable
+SELECT * FROM @RollingTable
 GO
 
 
 
 --8
 DECLARE @RandomArtist nvarchar(250)
-SELECT TOP 1 @RandomArtist = Name FROM Artist ORDER BY NEWID()
-PRINT(@RandomArtist)
+SET @RandomArtist = (SELECT TOP 1 Name FROM Artist ORDER BY NEWID())
+PRINT @RandomArtist
 GO 7
 
 
 --9
-PRINT('First Statement')
-
+PRINT 'First Statement'
 GOTO Third
-
-Third:
-PRINT('Third Statement')
-GOTO SECOND
-
 Second:
-PRINT('Second Statement')
+PRINT 'Second Statement'
 RETURN
+Third:
+PRINT 'Third Statement'
+GOTO SECOND
 GO
 
 
 --10
-DECLARE @EmployeeT AS table (FirstName nvarchar(50),LastName nvarchar(50),EmployeeId int,City nvarchar(50))
-DECLARE @FirstName AS nvarchar(50)
-DECLARE @LastName AS nvarchar(50)
+DECLARE @FirstName AS nvarchar(25)
+DECLARE @LastName AS nvarchar(25)
 DECLARE @EmployeeId AS int
 DECLARE @City AS nvarchar(50)
 DECLARE @Counter int = 1
 DECLARE @MaxId int
-SELECT @MaxId = COUNT(*) FROM Employee
+SELECT @MaxId = MAX(EmployeeId) FROM Employee
 WHILE (@Counter <= @MaxId)
 BEGIN
 SELECT
@@ -155,9 +150,7 @@ SELECT
     ,@City = City
 FROM Employee
 WHERE EmployeeId = @Counter
-INSERT INTO @EmployeeT
-VALUES(@FirstName, @LastName, @EmployeeId, @City)
-RAISERROR('%s %s has and ID of %d and lives in the city of %s.', 10, 1, @FirstName, @LastName, @EmployeeId, @City) WITH NOWAIT
+RAISERROR('%s %s has and ID of %d and lives in the city of %s.', 10, 1, @FirstName, @LastName, @EmployeeId, @City)
 SET @Counter += 1
 END
 
